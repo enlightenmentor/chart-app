f06f3ba32abf1dfe5dbf71f5f3be6d5b7308d2d0
enlightenmentor <fales.tom.1994@gmail.com>
2019-03-10 14:04:57 +0200

    random data for chart generated

--- src/components/app-root.js
+++ src/components/app-root.js
@@ -1,6 +1,40 @@
 import { LitElement, html, css } from 'lit-element';
 import './t-radio-button.js';
 
+function generateRandomData() {
+  function randInt(n) {
+    return Math.round((Math.random()*n));
+  }
+
+  function getCurrentYearDates() {
+    const NOW = new Date();
+    const TIMEZONE_OFFSET = NOW.getTimezoneOffset()*60*1000;
+    const DAY_DURATION = 24*60*60*1000;
+    const MONTHS_SHORT = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Now","Dec"];
+
+    let currentYear = NOW.getFullYear().toString();
+    let yearTimestamp = new Date(currentYear).getTime()+TIMEZONE_OFFSET;
+    let daysLength = Math.floor((NOW.getTime()-yearTimestamp) / DAY_DURATION);
+
+    return new Array(daysLength).fill({}).map((day, i) => {
+      let date = new Date(yearTimestamp + i*DAY_DURATION);
+      return `${MONTHS_SHORT[date.getMonth()]} ${date.getDate()}`;
+    });
+  }
+
+  function randomFollowersForDates(dates, max) {
+    return dates.map(date => {
+      return {
+        date,
+        joined: randInt(250),
+        left: randInt(100)
+      }
+    });
+  }
+
+  return randomFollowersForDates(getCurrentYearDates());
+}
+
 class AppRoot extends LitElement {
   static get styles() {
     return css`
@@ -35,6 +69,17 @@ class AppRoot extends LitElement {
         <t-radio-button class="app__left-button" checked>Left</t-radio-button>
       </div>
     `
+  }
+
+  static get properties() {
+    return {
+      data: Object
+    }
+  }
+
+  constructor() {
+    super();
+    this.data = generateRandomData();
   }
 }
 
