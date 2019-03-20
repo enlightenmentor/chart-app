01f21e8bac000991a25d1503f129e39b0d41fa0b
enlightenmentor <fales.tom.1994@gmail.com>
2019-03-20 12:59:16 +0200

    adding PWA features

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,30 @@
 const path = require('path');
-const defaultConfig = require('@open-wc/building-webpack/default-config');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+const createDefaultConfig = require('@open-wc/building-webpack/default-config');
 
-module.exports = defaultConfig({
+const defaultConfig = createDefaultConfig({
   indexJS: path.resolve(__dirname, './src/index.js'),
 });
+
+const config = Object.assign({}, defaultConfig, {
+  plugins: defaultConfig.plugins.concat([
+    new CopyWebpackPlugin([{
+      from: 'assets/',
+      to: 'assets/',
+    }, {
+      from: 'manifest.webmanifest',
+      to: 'manifest.webmanifest',
+    }]),
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'telegram-chart-app',
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: 'sw.js',
+      minify: true,
+      navigateFallback: '/',
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+    })
+  ])
+});
+
+module.exports = config;
